18c391cd888420a31d681dccf8955387196c2b08
Degisew <jegisew21@gmail.com>
2023-02-21 22:07:04 +0300

    Added resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users do
+  resources :foods do
+  resources :recipes
+  resources :recipe_foods
+  end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
