f4ca69b7929a361bbce400f4d7c30b30b8e1bc67
Degisew <jegisew21@gmail.com>
2023-03-05 20:40:01 +0300

    fix query issue

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.all.includes(:user).where(public: 'true').order(created_at: :desc)
+    @public_recipes = Recipe.all.where(public: true).order(created_at: :desc)
   end
 end
